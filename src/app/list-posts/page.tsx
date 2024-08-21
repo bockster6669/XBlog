5bc872dfd0513090428db0b290ae8218446ccece
daviddvorszky <dv98david@gmail.com>
2024-08-21 18:59:38 +0200

    feat(ui): implement paginated posts list with table and pagination controls

---
+++ src/app/list-posts/page.tsx
@@ -0,0 +1,12 @@
+import ListPosts from '@/components/list-posts/list-posts';
+import React from 'react';
+
+export default function page() {
+  return (
+    <main className="w-[1100px] mx-auto flex justify-center">
+      <div>
+        <ListPosts />
+      </div>
+    </main>
+  );
+}
