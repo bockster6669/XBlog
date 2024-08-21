9ef78212e217cf8139a5e7365ff2968f42d2f048
Versus Development <bockster6669@gmail.com>
2024-08-22 00:19:40 +0300

    merged post folder route file into posts folder route file

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,43 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '../../../../prisma/db';
+import { CreatePostSchema } from '../../../../resolvers/create-post-form.resolver';
+
+export async function POST(request: NextRequest, response: NextResponse) {
+  const body = await request.json();
+  const validFields = CreatePostSchema.safeParse(body);
+
+  if (!validFields.success)
+    return NextResponse.json({ error: 'Not valid fields' }, { status: 400 });
+
+  const { content, title, category: categoryName } = validFields.data;
+
+  const category = await db.category.upsert({
+    where: { name: categoryName },
+    update: {},
+    create: { name: categoryName },
+  });
+
+  try {
+    await db.post.create({
+      data: {
+        title,
+        content,
+        category: {
+          connect: { id: category.id },
+        },
+        author: {
+          connect: { id: 2 },
+        },
+      },
+    });
+    return NextResponse.json({ success: 'Post created' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Error accured while creating a post' },
+      { status: 400 }
+    );
+  }
+}
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
