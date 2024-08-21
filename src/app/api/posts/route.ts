c4fed1c26364db18937db375036ec0a0e9c2df18
daviddvorszky <dv98david@gmail.com>
2024-08-21 18:59:22 +0200

    feat(api): implement pagination in posts API with skip and take parameters

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,15 +1,32 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { db } from '../../../../prisma/db';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const skip = parseInt(searchParams.get('skip') || '0', 10);
+  const take = parseInt(searchParams.get('take') || '10', 10);
+
   try {
+    // Get the total count of posts
+    const totalPosts = await db.post.count();
+
+    // Fetch the posts with pagination
     const posts = await db.post.findMany({
+      skip: skip,
+      take: take,
       include: {
         category: true,
         author: true,
       },
     });
-    return NextResponse.json(posts, { status: 200 });
+
+    // Return the posts along with pagination info
+    return NextResponse.json({
+      posts: posts,
+      totalPosts: totalPosts,
+      currentPage: Math.floor(skip / take) + 1,
+      totalPages: Math.ceil(totalPosts / take),
+    }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
