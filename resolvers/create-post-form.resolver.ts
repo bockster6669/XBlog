ee720679c08cae3e0dd418f9c35af425ddc2b76d
Versus Development <bockster6669@gmail.com>
2024-09-01 14:11:23 +0300

    update

--- resolvers/create-post-form.resolver.ts
+++ resolvers/create-post-form.resolver.ts
@@ -5,8 +5,7 @@ export const CreatePostSchema = z.object({
   content: z.string().min(5),
   excerpt: z.string().min(5),
   category: z
-    .string()
-    .min(1, { message: 'You have to choose a category for this post' }),
+    .string(),
   categories: z.array(z.object({ name: z.string() })).min(1),
 });
 export type CreatePostFormValues = z.infer<typeof CreatePostSchema>;
