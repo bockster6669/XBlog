255b67c3fc5c7cce63f30b54bfb8d091f1d8fd6c
Versus Development <bockster6669@gmail.com>
2024-09-29 16:20:07 +0300

    update

--- src/app/(main)/create-post/page.tsx
+++ src/app/(main)/create-post/page.tsx
@@ -1,4 +1,4 @@
-import CreatePostForm from '@/lib/features/posts/create-post-form';
+import CreatePostForm from '@/lib/features/posts/create-post-form/create-post-form';
 import React from 'react';
 
 export default function Page() {
